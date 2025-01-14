6e92aece1680b657fcc324c2a7107074afa53100
hemankpatwal <98538477+hemankpatwal@users.noreply.github.com>
2025-01-14 23:48:58 +0530

    create and update post - added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from.models import Post
-from django.views.generic import ListView
+from django.views.generic import (
+    ListView,
+    DetailView, 
+    CreateView,
+    UpdateView,
+    )
 
 
 def home(request):
@@ -15,6 +21,25 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
