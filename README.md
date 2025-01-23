a4adbd365907ab3096959f95ce28409d8cea692a
hemankpatwal <98538477+hemankpatwal@users.noreply.github.com>
2025-01-23 18:40:42 +0530

    Added README.md file

---
+++ README.md
@@ -0,0 +1,20 @@
+# Django Blog Site
+
+The Django Blog Site is a simple blogging platform where users can create, read, update, and delete blog posts. The website includes features for user authentication, post management, and an easy-to-use interface to interact with blog content. Built with Django, it serves as a robust and scalable solution for managing and displaying blog content.
+
+## Features
+
+- **User**: Authentication: User registration, login, and logout functionality.
+- **Post Management**: Create, update, delete, and view blog posts.
+- **Comment Section**: Allow users to leave comments on blog posts.
+- **Responsive Design**: Optimized for viewing on desktop and mobile devices.
+
+## Requirements
+
+To run this project, you need:
+
+Python 3.7 or higher
+Django (v5.1.5 or higher)
+SQLite (default database; can be changed to PostgreSQL, MySQL, etc.)
+Pillow (for image support in blog posts)
+
--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+import os
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,15 +10,22 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
     
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
 
-        img = Image.open(self.image.path)
+        if not self.pk and not self.image:
+            self.image = 'default.jpg'
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+
+        super().save(*args, **kwargs)
+
+        if self.image and os.path.exists(self.image.path):
+
+            img = Image.open(self.image.path)
+
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
 
 
 
